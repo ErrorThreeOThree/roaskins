88afb8be06fbd893ebc19a0d59085f1c79e4aa6b
Julian Hartmer <j.hartmer@googlemail.com>
2021-01-23 04:11:34 +0100

    version 1.0. Add Readme. Adding documentation link

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Rivals of Aether skin package
+**Rivals of Aether skin package**
+
 This is a fan-made package to generate skins for the game Rivals of Aether.
+
+**Features**
+
+- Generate new skins from color code
+- Generate skins with random color
+- Get the valid in-game color code of skins
+- Generate preview images
+- Show preview images in window
+- Print preview images to file
+
+**Documentation**
+
+You can find the documentation here: https://htmlpreview.github.io/?https://github.com/ErrorThreeOThree/roaskins/blob/master/docs/build/html/index.html
+
+**Example Application: Discord chat bot**
+
+I have provided an example application using this library. This application is a discord chat bot that can generate random skins and post preview images in discord. You can find the source code here: https://github.com/ErrorThreeOThree/ROASkinBot
--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from distutils.core import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
 setup(
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     name = 'roaskins',
     packages = ['roaskins'],
-    version = '0.42',
+    version = '1.0',
     license = 'MIT',
     description = 'Rivals of Aether skin and color code library',
     author = 'Julian Hartmer',
     author_email = 'j.hartmer@googlemail.com',
     url = 'https://github.com/ErrorThreeOThree/roaskins',
     keywords = ['roa', 'Rivals', 'of', 'Aether', 'skin'],
-    package_data={'': ['data/*/*']},
+    package_data={'': ['data/*/*', 'README.md']},
     include_package_data=True,
     install_requires = [
         'opencv-python',
